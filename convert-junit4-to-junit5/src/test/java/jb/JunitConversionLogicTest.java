90ba46aa8f7d7f10df82742d85b044728a8c16eb
Jeanne Boyarsky <nyjeanne@gmail.com>
2017-09-02 18:44:19 -0400

    gave up on trying to preserve formatting and switched to a proper parser

--- convert-junit4-to-junit5/src/test/java/jb/JunitConversionLogicTest.java
+++ convert-junit4-to-junit5/src/test/java/jb/JunitConversionLogicTest.java
@@ -8,95 +8,162 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
 
+import com.github.javaparser.*;
+import com.github.javaparser.ast.*;
+
 class JunitConversionLogicTest {
+	
+	private static String convertWhitespaceForJavaParser(String string) {
+		CompilationUnit cu = JavaParser.parse(string);
+		return cu.toString();
+	}
+
+	// wrap in class so well formed for parser
+	private static void assertWrappingInClass(String code, String expected) {
+		String prefix = "public class A { ";
+		String postfix = " }";
+		String codeWrapped = prefix + code + postfix;
+		String expectedWrapped = prefix + expected + postfix;
+		String actual = JunitConversionLogic.convert(codeWrapped);
+		assertEquals(convertWhitespaceForJavaParser(expectedWrapped), actual);
+	}
+
+	// wrap in class/method so well formed for parser
+	public static void assertAfterWrappingInMethod(String code, String expected) {
+		String prefix = "public class A { public void m() { ";
+		String postfix = " }}";
+		String codeWrapped = prefix + code + postfix;
+		String expectedWrapped = prefix + expected + postfix;
+		String actual = JunitConversionLogic.convert(codeWrapped);
+		assertEquals(convertWhitespaceForJavaParser(expectedWrapped), actual);
+	}
+
+	// add class after import so well formed for parser
+	private static void assertAfterAddingClassAfter(String code, String expected) {
+		String postfix = "public class A {}";
+		String codeWrapped = code + postfix;
+		String expectedWrapped = expected + postfix;
+		String actual = JunitConversionLogic.convert(codeWrapped);
+		assertEquals(convertWhitespaceForJavaParser(expectedWrapped), actual);
+	}
+
+	// wrap in class/method so well formed for parser
+	private static void assertAfterWrappingInMethod(String originalImport, String originalMethod, String expectedImport,
+			String expectedMethod) {
+		String codeWrapped = originalImport + "public class A { public void m() { " + originalMethod + "}}";
+		String expectedWrapped = expectedImport + "public class A { public void m() { " + expectedMethod + "}}";
+		String actual = JunitConversionLogic.convert(codeWrapped);
+		assertEquals(convertWhitespaceForJavaParser(expectedWrapped), actual);
+	}
+
+	// ------------------------------------------------------------------
 
 	@Test
 	void importWildcardStaticAssertions() {
-		String actual = JunitConversionLogic.convert("import static org.junit.Assert.*;");
-		assertEquals("import static org.junit.jupiter.api.Assertions.*;", actual);
+		String code = "import static org.junit.Assert.*;";
+		String expected = "import static org.junit.jupiter.api.Assertions.*;";
+		assertAfterAddingClassAfter(code, expected);
 	}
 
 	@Test
 	void specificStaticAssertions() {
-		String actual = JunitConversionLogic.convert("import org.junit.Assert; Assert.assertTrue;");
-		assertEquals("import org.junit.jupiter.api.Assertions; Assertions.assertTrue;", actual);
+		String originalImport = "import org.junit.Assert;";
+		String originalMethod = "Assert.assertTrue(a,b);";
+		String expectedImport = "import org.junit.jupiter.api.Assertions;";
+		String expectedMethod = "Assertions.assertTrue(b,a);";
+		assertAfterWrappingInMethod(originalImport, originalMethod, expectedImport, expectedMethod);
 	}
 
 	@Test
 	void specificStaticFail() {
-		String actual = JunitConversionLogic.convert("import org.junit.Assert; Assert.fail;");
-		assertEquals("import org.junit.jupiter.api.Assertions; Assertions.fail;", actual);
+		String originalImport = "import org.junit.Assert;";
+		String originalMethod = "Assert.fail(a);";
+		String expectedImport = "import org.junit.jupiter.api.Assertions;";
+		String expectedMethod = "Assertions.fail(a);";
+		assertAfterWrappingInMethod(originalImport, originalMethod, expectedImport, expectedMethod);
 	}
 
 	@Test
 	void specificFullyQualifiedStaticAssertions() {
-		String actual = JunitConversionLogic.convert("org.junit.Assert.assertTrue;");
-		assertEquals("org.junit.jupiter.api.Assertions.assertTrue;", actual);
+		String originalImport = "import org.junit.Assert;";
+		String originalMethod = "org.junit.Assert.assertTrue(a,b);";
+		String expectedImport = "import org.junit.jupiter.api.Assertions;";
+		String expectedMethod = "org.junit.jupiter.api.Assertions.assertTrue(b,a);";
+		assertAfterWrappingInMethod(originalImport, originalMethod, expectedImport, expectedMethod);
 	}
 
 	@Test
 	void importWildcardStaticAssumptions() {
-		String actual = JunitConversionLogic.convert("import static org.junit.Assume.*;");
-		assertEquals("import static org.junit.jupiter.api.Assumptions.*;", actual);
+		String originalImport = "import static org.junit.Assume.*;";
+		String expectedImport = "import static org.junit.jupiter.api.Assumptions.*;";
+		assertAfterAddingClassAfter(originalImport, expectedImport);
 	}
 
 	@Test
 	void specificStaticAssumptions() {
-		String actual = JunitConversionLogic.convert("import org.junit.Assume; Assume.assumeTrue;");
-		assertEquals("import org.junit.jupiter.api.Assumptions; Assumptions.assumeTrue;", actual);
+		String originalImport = "import org.junit.Assume;";
+		String originalMethod = "Assume.assumeTrue(a,b);";
+		String expectedImport = "import org.junit.jupiter.api.Assumptions;";
+		String expectedMethod = "Assumptions.assumeTrue(b,a);";
+		assertAfterWrappingInMethod(originalImport, originalMethod, expectedImport, expectedMethod);
 	}
 
 	@Test
 	void specificFullyQualifiedStaticAssumptions() {
-		String actual = JunitConversionLogic.convert("org.junit.Assume.assumeFalse;");
-		assertEquals("org.junit.jupiter.api.Assumptions.assumeFalse;", actual);
+		String originalImport = "import static org.junit.Assume.assumeFalse;";
+		String expectedImport = "import static org.junit.jupiter.api.Assumptions.assumeFalse;";
+		assertAfterAddingClassAfter(originalImport, expectedImport);
 	}
 
 	@Test
 	void importWildcardMainPackage() {
-		String actual = JunitConversionLogic.convert("import org.junit.*;");
-		assertEquals("import org.junit.jupiter.api.*;", actual);
+		String originalImport = "import org.junit.*;";
+		String expectedImport = "import org.junit.jupiter.api.*;";
+		assertAfterAddingClassAfter(originalImport, expectedImport);
 	}
 
 	@Test
 	void importJunit38Wildcard() {
-		String actual = JunitConversionLogic.convert("import junit.framework.*;");
-		assertEquals("import org.junit.jupiter.api.*;", actual);
+		String originalImport = "import junit.framework.*;";
+		String expectedImport = "import org.junit.jupiter.api.*;";
+		assertAfterAddingClassAfter(originalImport, expectedImport);
 	}
 
 	@Test
 	void importJunit38Assert() {
-		String actual = JunitConversionLogic.convert("import junit.framework.Assert;");
-		assertEquals("import org.junit.jupiter.api.Assertions;", actual);
+		String originalImport = "import junit.framework.Assert;";
+		String expectedImport = "import org.junit.jupiter.api.Assertions;";
+		assertAfterAddingClassAfter(originalImport, expectedImport);
 	}
 
 	@Test
-	void specificMainPackage() {
-		String actual = JunitConversionLogic.convert("org.junit.Test");
-		assertEquals("org.junit.jupiter.api.Test", actual);
+	void specificTestAnnotation() {
+		String annotation = "import org.junit.Test;";
+		String expected = "import org.junit.jupiter.api.Test;";
+		assertAfterAddingClassAfter(annotation, expected);
 	}
 
 	@Test
 	void assertThat() {
-		String actual = JunitConversionLogic.convert("org.junit.Assert.assertThat;");
-		assertEquals("org.hamcrest.MatcherAssert.assertThat;", actual);
+		String code = "org.junit.Assert.assertThat(a, b);";
+		String expected = "org.hamcrest.MatcherAssert.assertThat(a, b);";
+		assertAfterWrappingInMethod(code, expected);
 	}
 
 	@Test
 	void newImportForAssertThat() {
-		String actual = JunitConversionLogic.convert("import static org.junit.Assert.*;\n"
-				+ "assertThat(xxx);");
-		assertEquals("import static org.hamcrest.MatcherAssert.assertThat;\n"
-				+ "import static org.junit.jupiter.api.Assertions.*;\n"
-				+ "assertThat(xxx);", actual);
+		String originalImport = "import static org.junit.Assert.*;\n";
+		String originalMethod = "assertThat(xxx);";
+		String expectedImport = "import static org.hamcrest.MatcherAssert.assertThat;\n"
+				+ "import static org.junit.jupiter.api.Assertions.*;\n";
+		assertAfterWrappingInMethod(originalImport, originalMethod, expectedImport, originalMethod);
 	}
 
 	@Test
 	void importAlreadyPresentForAssertThat() {
-		String code = "import static org.hamcrest.MatcherAssert.assertThat;\n"
-				+ "assertThat(xxx);";
-		String actual = JunitConversionLogic.convert(code);
-		assertEquals(code, actual);
+		String originalImport = "import static org.hamcrest.MatcherAssert.assertThat;\n";
+		String originalMethod = "assertThat(xxx);";
+		assertAfterWrappingInMethod(originalImport, originalMethod, originalImport, originalMethod);
 	}
 
 	@Test
@@ -110,23 +177,38 @@ class JunitConversionLogicTest {
 	void singleAssertion() {
 		String code = "assertTrue(message, actual);";
 		String expected = "assertTrue(actual, message);";
-		String actual = JunitConversionLogic.convert(code);
-		assertEquals(expected, actual);
+		assertAfterWrappingInMethod(code, expected);
 	}
 
 	@Test
 	void mutlipleAssertions() {
 		String code = "assertTrue(message, actual);";
 		String expected = "assertTrue(actual, message);";
-		String actual = JunitConversionLogic.convert(code);
-		assertEquals(expected, actual);
+		assertAfterWrappingInMethod(code, expected);
 	}
 
 	@Test
 	void nonJunitMethodCall() {
 		String code = "doWork(message, actual);";
-		String actual = JunitConversionLogic.convert(code);
-		assertEquals(code, actual);
+		assertAfterWrappingInMethod(code, code);
+	}
+
+	// -------------------------------------------------------
+
+	@Test
+	void assertInFirstLineOfMethod() {
+		String code = "public void method() { assertTrue(message, actual); }";
+		String expected = "public void method() { assertTrue(actual, message); }";
+		assertWrappingInClass(code, expected);
+	}
+
+	@Test
+	void assertAfterCommentOnPreviousLine() {
+		String code = "public void method() { assertTrue(message, actual); // comment \n"
+				+ "assertTrue(message, actual); }";
+		String expected = "public void method() { assertTrue(actual, message); // comment \n"
+				+ "assertTrue(actual, message); }";
+		assertWrappingInClass(code, expected);
 	}
 
 	// -------------------------------------------------------
@@ -134,11 +216,13 @@ class JunitConversionLogicTest {
 	@ParameterizedTest
 	@MethodSource("annotationsProvider")
 	void newAnnotationNames(String oldAnnotationName, String newAnnotationName) {
-		String fullyQualified = JunitConversionLogic.convert("org.junit." + oldAnnotationName);
-		assertEquals("org.junit.jupiter.api." + newAnnotationName, fullyQualified);
+		String code = "import org.junit." + oldAnnotationName + ";\n"
+				+ "public class A { \n@" + oldAnnotationName + "\npublic void m() { }}";
+		String expected = "import org.junit.jupiter.api." + newAnnotationName + ";\n"
+				+ "public class A { \n@" + newAnnotationName + "\npublic void m() { }}";
 
-		String annotationActual = JunitConversionLogic.convert("@" + oldAnnotationName);
-		assertEquals("@" + newAnnotationName, annotationActual);
+		String actual = JunitConversionLogic.convert(code);
+		assertEquals(convertWhitespaceForJavaParser(expected), actual);
 	}
 
 	static Stream<Arguments> annotationsProvider() {
