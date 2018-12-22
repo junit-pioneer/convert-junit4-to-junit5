22b14435b155e364d72693a9f34576f1fd4c713b
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-12-22 13:10:52 +0100

    separate file system traversal from migration

--- convert-junit4-to-junit5/src/main/java/jb/Updater.java
+++ convert-junit4-to-junit5/src/main/java/jb/Updater.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Converts a file or nested directories to use JUnit 5 syntax where possible.
@@ -46,24 +48,30 @@ public class Updater {
      * @param path a file or directory to update recursively
      */
     void update(Path path) throws IOException {
+        List<Path> filesToMigrate;
         if (path.toFile().isFile()) {
-            updateSingleFile(path);
+            filesToMigrate = Collections.singletonList(path);
         } else {
-            ConversionReport report = convertAll(path);
-            System.out.println(report.print());
+            filesToMigrate = javaFilesIn(path);
         }
+        ConversionReport report = convertAll(filesToMigrate);
+        System.out.println(report.print());
     }
 
-    private ConversionReport convertAll(Path path) throws IOException {
-        List<ConversionResult> result;
+    private ConversionReport convertAll(List<Path> filesToMigrate) {
+        List<ConversionResult> result = filesToMigrate.stream()
+                .map(this::updateSingleFile)
+                .collect(toList());
+        return new ConversionReport(result);
+    }
+
+    private List<Path> javaFilesIn(Path path) throws IOException {
         try (Stream<Path> stream = Files.walk(path)) {
             // only update java files
-            result = stream.filter(p -> p.toFile().isFile())
+            return stream.filter(p -> p.toFile().isFile())
                     .filter(p -> p.toString().endsWith(".java"))
-                    .map(this::updateSingleFile)
-                    .collect(Collectors.toList());
+                    .collect(toList());
         }
-        return new ConversionReport(result);
     }
 
     private ConversionResult updateSingleFile(Path path) {
